325fef3754674d850f84ba53556901960398ccba
janakachamod <janakachamod1234@gmail.com>
2023-03-17 20:37:48 +0530

    final commit

---
+++ app/src/main/java/com/assessment3/holidayappnextversion/Destination.kt
@@ -0,0 +1,11 @@
+package com.assessment3.holidayappnextversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Destination : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination)
+    }
+}
